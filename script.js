aff8063527230e69fddc1d17406351cdd85a9f1a
kelvinyhf <yhf719@hotmail.com>
2026-07-31 14:18:57 +0800

    add: add item and local storage

--- script.js
+++ script.js
@@ -1,13 +1,31 @@
+// Variables
+const STORAGE_KEY = "TICK_V1";
+
 // State
 const state = PetiteVue.reactive({
-  items: [
-    { title: 'Hello World!', desc: 'My first item!', completed: false },
-    { title: 'Hello You!', desc: 'My second item!', completed: false },
-    { title: 'Hello Me!', desc: 'My third item!', completed: false },
-    { title: 'Hello Gemini!', desc: 'My fourth item!', completed: false },
-    { title: 'Hello David!', desc: 'My fifth item!', completed: false },
-    { title: 'Hello Guys!', desc: 'My sixth item!', completed: false }
-  ]
+  
+  // Items Object
+  items: /* JSON.parse(localStorage.getItem(STORAGE_KEY)) || */[
+    { title: 'Item 1', desc: 'My first item!', completed: false },
+    { title: 'Item 2', desc: 'My second item!', completed: false },
+    { title: 'Item 3', desc: 'My third item!', completed: false }
+  ],
+  
+  // Save
+  save() {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(this.items));
+  },
+  
+  // Add Item
+  addItem() {
+    this.items.push({
+      title: '',
+      desc: '',
+      completed: false
+    });
+    this.save();
+  }
+  
 });
 
 // Create App
